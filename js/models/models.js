9ada0298a61e8cc7cf6b587d329ba1eb93bf8cf9
Loic Coenen <ijwtcymb@gmail.com>
2018-10-28 19:20:26 +0000

    Added the built

---
+++ js/models/models.js
@@ -0,0 +1,12 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./ride"));
+__export(require("./user"));
+__export(require("./prospect"));
+__export(require("./message"));
+__export(require("./link"));
+__export(require("./rating"));
+//# sourceMappingURL=models.js.map
